617ae49e1b44f7820c027afa4ac8290d88da6617
vagrant <vagrant@localhost.localdomain>
2020-01-25 23:51:52 +0900

    initial commit

---
+++ Review/src/demo/Main.java
@@ -0,0 +1,22 @@
+package demo;
+
+import java.util.ArrayList;
+
+import entity.Member;
+import service.MemberServiceImpl;
+
+public class Main{
+
+    public static void main(String[] args){
+        MemberServiceImpl service = new MemberServiceImpl();
+        System.out.println(service.greet(2));
+        System.out.println(service.getAll());
+        ArrayList<Member> list = service.getAll();
+        for(Member member : list){
+            System.out.println(member.getId() + "," + member.getName() + "," + member.getEmail());
+        }
+        // 3èÕÅE11ÇÃâ€ëË
+        System.out.println(service.sumOf(3, 5));
+    }
+
+}
